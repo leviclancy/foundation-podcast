fae1d7413951d7de99fb8d776c6b117d47e6374f
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-04-09 16:52:12 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?
+<? // if RSS
 
 ?>
+
+<html>
+<head>
+<link rel='alternate' type='application/rss+xml' title='Podcast' href='podcast.ours.foundatoin/podcast.rss' />
+<title>Museum of Ours Podcast</title>
+</head>
+<body>
+<h1>Museum of Ours Podcast</h1>
+<p>Museum of Ours</p>
+</body>
+</html>
