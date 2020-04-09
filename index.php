37f41074e192f2f855185f7b4a5a27c1c83716cc
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-04-09 16:53:53 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <? 
 
-// https://developers.google.com/search/docs/guides/podcast-guidelines
-// Podcast must have a homepage
-// Podcast must have an RSS feed
-// Podcast must have at least one episode
+// Podcast has three requirements https://developers.google.com/search/docs/guides/podcast-guidelines
+	// Podcast must have a homepage https://developers.google.com/search/reference/podcast/homepage-requirements
+	// Podcast must have an RSS feed https://developers.google.com/search/reference/podcast/rss-feed
+	// Podcast must have at least one episode https://developers.google.com/search/reference/podcast/rss-feed#episode-level
 
 // if RSS
 	
