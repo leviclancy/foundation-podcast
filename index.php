7d8a561ef97fe6b2f75278005571e6009594590b
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-04-09 19:14:10 -0700

    Update index.php

--- index.php
+++ index.php
@@ -8,13 +8,22 @@
 include_once('config.php');
 
 if (empty($_REQUEST['access'])): $_REQUEST['access'] = null; endif;
+if (empty($_REQUEST['episode'])): $_REQUEST['episode'] = null; endif;
 
 // Give us the install routine
 if ($_REQUEST['access'] == "install"):
 
+	// create database
+
 	// create users table
 
 	// create podcasts table
+	// episode_id
+	// episode_title
+	// episode_description
+	// episode_pubdate
+	// episode_url
+	// episode_duration
 
 	// if nobody in users table then create a user
 
@@ -30,7 +39,7 @@ if ($_REQUEST['access'] == "xhr-install"):
 
 $login = null;
 
-if (empty($login) && in_array($_REQUEST['access'], ["xhr-login", "xhr-"logout", "xhr-save", "admin"])):
+if (empty($login) && in_array($_REQUEST['access'], ["xhr-login", "xhr-"logout", "xhr-account", "xhr-add", "xhr-update", "admin"])):
 	// json of invalid login
 	exit; endif;
 
@@ -50,8 +59,22 @@ if ($_REQUEST['access'] == "xhr-logout"):
 
 // Check the database
 
-// Give us the save xhr
-if ($_REQUEST['access'] == "xhr-save"):
+// Give us the account xhr
+if ($_REQUEST['access'] == "xhr-account"):
+
+	// View podcasts
+
+	exit; endif;
+
+// Give us the add xhr
+if ($_REQUEST['access'] == "xhr-add"):
+
+	// View podcasts
+
+	exit; endif;
+
+// Give us the update xhr
+if ($_REQUEST['access'] == "xhr-update"):
 
 	// View podcasts
 
@@ -60,6 +83,16 @@ if ($_REQUEST['access'] == "xhr-save"):
 // Give us the admin panel
 if ($_REQUEST['access'] == "admin"):
 
+	amp_header("Admin");
+
+	if (array_key_exists($_REQUEST['episode'], $episodes_array)):
+
+		// Go ahead and provide the edit for just the podcast
+	
+		endif;
+
+	// Add podcast
+
 	// View podcasts
 
 	// 
@@ -67,7 +100,21 @@ if ($_REQUEST['access'] == "admin"):
 	// 
 	// Change password
 
-	exit; endif;
+	footer(); endif;
+
+// Give us the episode
+if (array_key_exists($_REQUEST['episode'], $episode_array)):
+
+	// Return the audio files
+
+	endif;
+
+// Give us the JSON
+if ($_REQUEST['access'] == "json"):
+
+	// JSON of all the episodes
+
+	endif;
 
 // Give us the RSS
 if ($_REQUEST['access'] == "rss"):
@@ -98,18 +145,25 @@ if ($_REQUEST['access'] == "rss"):
 		</channel>
 		</rss>
 
-<? exit; endif; ?>
+<? exit; endif;
 
-<html>
-<head>
-<link rel='alternate' type='application/rss+xml' title='Podcast' href='podcast.ours.foundatoin/podcast.rss' />
-<title><? echo $title; ?></title>
-</head>
-<body>
-<h1><? echo $title; ?></h1>
-<p><? echo $description; ?></p>
-<p>RSS feed: https://<? echo $link; ?>/?view=rss</p>
+function amp_header($title_temp) {
+	echo "<html>";
+	echo "<head>";
+	echo "<link rel='alternate' type='application/rss+xml' title='Podcast' href='podcast.ours.foundatoin/podcast.rss' />";
+	echo "<title>". $title_temp ."</title>";
+	echo "</head>";
+	echo "<body>";
+	}
+						   
+function amp_footer() {
+	echo "</body></html>";
+	exit;
+	}
 	
-List of amp-audio for each episode
-</body>
-</html>
+amp_header($title);
+echo "<h1>". $title ."</h1>";
+echo "<p>". $description . "</p>";
+echo "<p>RSS feed: https://". $link ."/?view=rss</p>";
+echo "List of amp-audio for each episode";
+amp_footer(); ?>
