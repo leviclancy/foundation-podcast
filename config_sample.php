95b2c28d26a3877e977e892a962619a01705283f
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-04-09 17:15:46 -0700

    Create config_sample.php

---
+++ config_sample.php
@@ -0,0 +1,11 @@
+<?
+
+// Podcast info
+  // Museum of Ours Podcast
+$title = ""; 
+$author = ""; // Museum of Ours
+$description = ""; // Museum of Ours brings you...
+$link = ""; // podcast.ours.foundation
+$language = ""; // en-us
+
+?>
