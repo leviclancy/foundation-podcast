35d07e6a262bc990899fd4fd73f8fcdce284cb38
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-04-17 01:46:51 -0700

    Update index.php

--- index.php
+++ index.php
@@ -195,30 +195,30 @@ if ($request_access == "rss"):
 
 	// 
 
-	?>
+	echo '<?xml version="1.0" encoding="UTF-8"?>';
+	echo '<rss version="2.0" xmlns:googleplay="http://www.google.com/schemas/play-podcasts/1.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">';
+	echo '<channel>';
 
-	<?xml version="1.0" encoding="UTF-8"?>
-	<rss version="2.0" xmlns:googleplay="http://www.google.com/schemas/play-podcasts/1.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">
-	<channel>
-		<title><? echo $title; ?></title>
-		<googleplay:author><? echo $author; ?></googleplay:author>
-		<description><? echo $description; ?></description>
-		<googleplay:image href="http://www.example.com/podcasts/dafnas-zebras/img/dafna-zebra-pod-logo.jpg"/>
-		<language><? echo $language ?></language>
-		<link><? echo $link; ?></link>
-		<item>
+		echo '<title>' . $title .'</title>';
+		echo '<googleplay:author>'. $author .'</googleplay:author>';
+		echo '<description>'. $description .'</description>';
+		echo '<googleplay:image href="http://www.example.com/podcasts/dafnas-zebras/img/dafna-zebra-pod-logo.jpg"/>';
+		echo '<language>'. $language .'</language>';
+		echo '<link>'. $link .'</link>';
+
+		echo '<item>
 			<title>Top 10 myths about caring for a zebra</title>
 			<description>Here are the top 10 misunderstandings about the care, feeding, and breeding of these lovable striped animals.</description>
 			<pubDate>Tue, 14 Mar 2017 12:00:00 GMT</pubDate>
 			<enclosure url="https://www.example.com/podcasts/dafnas-zebras/audio/toptenmyths.mp3" type="audio/mpeg" length="34216300"/>
 			<itunes:duration>30:00</itunes:duration>
 			<guid isPermaLink="false">dzpodtop10</guid>
-   			</item>
-		</channel>
-		</rss>
+   			</item>';
 
-<? exit; endif;
-	
+		echo '</channel></rss>';
+
+	exit; endif;
+
 amp_header($title);
 echo "<h1>". $title ."</h1>";
 echo "<p>". $description . "</p>";
