ec2b6d0427ab974f03080092e3da8acf2bc3ffe7
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-04-17 01:20:42 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,90 @@
-<? 
-
-// Podcast has three requirements https://developers.google.com/search/docs/guides/podcast-guidelines
+<? // Podcast has three requirements https://developers.google.com/search/docs/guides/podcast-guidelines
 	// Podcast must have a homepage https://developers.google.com/search/reference/podcast/homepage-requirements
 	// Podcast must have an RSS feed https://developers.google.com/search/reference/podcast/rss-feed
 	// Podcast must have at least one episode https://developers.google.com/search/reference/podcast/rss-feed#episode-level
 
+session_start();
+mb_internal_encoding('UTF-8');
+mb_http_output('UTF-8');
+
 include_once('config.php');
+include_once('functions.php');
 
-if (empty($_REQUEST['access'])): $_REQUEST['access'] = null; endif;
-if (empty($_REQUEST['episode'])): $_REQUEST['episode'] = null; endif;
+$request_access = ( empty($_REQUEST['access']) ? null : $_REQUEST['access'] );
+$request_episode = ( empty($_REQUEST['episode']) ? null : $_REQUEST['episode'] );
 
-// Give us the install routine
-if ($_REQUEST['access'] == "install"):
+if ($request_access == "install"):
 
-	// create database
+	amp_header("Install");
 
-	// create users table
+	// Create postgres connection
+//	$postgres_connection = pg_connect("sslmode=verify-ca sslrootcert=server-ca.pem sslcert=client-cert.pem sslkey=client-key.pem host=$sql_host port=$sql_port dbname=$sql_database user=$sql_user password=$sql_password options='--client_encoding=UTF8'");
+	$postgres_connection = pg_connect("port=$sql_port dbname=$sql_database user=$sql_user password=$sql_password options='--client_encoding=UTF8'");
+	if (pg_connection_status($postgres_connection) !== PGSQL_CONNECTION_OK): json_result("error", "Failed database connection."); endif;
 
-	// create podcasts table
-	// episode_id
-	// episode_title
-	// episode_description
-	// episode_pubdate
-	// episode_url
-	// episode_duration
+	// Users table
+	$tables_array['users'] = [
+		"user_id"		=> "VARCHAR(100)", // Fixed user id
+		"username"		=> "VARCHAR(200)", // User-changeable username
+		"password_key"		=> "VARCHAR(200)", // Unique configuration key
+		"password_hash"		=> "VARCHAR(200)", // Hash of password
+		"magic_code"		=> "VARCHAR(200)", // Magic code for ephemeral login
+		"magic_expiration"	=> "VARCHAR(200)", // Magic code expiration time
+		"authenticator_key"	=> "VARCHAR(200)", // Authenticator configuration key key
+		"cookie_code"		=> "VARCHAR(200)", // Authenticator configuration key key
+		];
 
-	// if nobody in users table then create a user
+	$tables_array['description'] = [
+		"description_key"	=> "VARCHAR(100)", // title, author, description, language
+		"description_info"	=> "VARCHAR(500)",
+		];
 
-	exit; endif;
+	$tables_array['episodes'] = [
+		"episode_id"		=> "VARCHAR(100)",
+		"episode_title"		=> "VARCHAR(200)",
+		"episode_description"	=> "VARCHAR(500)",
+		"episode_pubdate"	=> "VARCHAR(100)",
+		"episode_duration"	=> "VARCHAR(100)",
+		"episode_file"		=> "TEXT",
+		];
+		
+	// Start generating the tables...
+	foreach($tables_array as $table_name => $columns_array):
 
-if ($_REQUEST['access'] == "xhr-install"):
+		// Prepare request...
+		$columns_schema = [];
+		foreach ($columns_array as $column_name => $column_type):
+			$columns_schema[] = $column_name." ".$column_type;
+			endforeach;
+		$columns_schema[0] .= " PRIMARY KEY";
+
+		// Execute query...
+		$sql_temp = "CREATE TABLE IF NOT EXISTS $table_name (". implode (", ", $columns_schema) .")";
+		$result = pg_query($postgres_connection, $sql_temp);
+		
+		// If it failed...
+		if (empty($result)): echo "<p>Error building table '".$table_name."' in '".$database."' database.<p>";
+		
+		// ... or if it succeeded...
+		else: echo "<p>Success building table '".$table_name."' in '".$database."' database.<p>"; endif;
+
+		endforeach;
+
+	// Pull up users if empty
+	$sql_temp = "SELECT * FROM users";
+	$result = pg_query($postgres_connection, $sql_temp);
+	if (empty($result)): echo "<p>Error building table '".$table_name."' in '".$database."' database.<p>";
+
+	while ($row = pg_fetch_row($result)) {
+		echo "Author: $row[0]  E-mail: $row[1]";
+		echo "<br />\n";
+		}
+
+	footer();
+
+	endif;
+
+if ($request_access == "xhr-install"):
 
 	// if there is a user then give error
 
@@ -37,21 +92,38 @@ if ($_REQUEST['access'] == "xhr-install"):
 
 	endif;
 
+
+
+$request_access_array = [
+	"xhr-login", "xhr-logout", "xhr-account", "xhr-add", "xhr-update", "admin", // For administration
+	"json", "rss", // Output the sitemap
+	];
+
+if (!(empty($request_access)) && in_array($request_access, $request_access_array)):
+	$connection_pdo = new PDO(
+		"mysql:host=$sql_host;dbname=$sql_database;charset=utf8mb4", 
+		$sql_user, 
+		$sql_password,
+		array(PDO::ATTR_TIMEOUT => 3, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)  // Number is in seconds
+		);
+	if (empty($connection_pdo)): echo "Could not connect to mySQL."; exit; endif;
+	endif;
+
 $login = null;
 
-if (empty($login) && in_array($_REQUEST['access'], ["xhr-login", "xhr-"logout", "xhr-account", "xhr-add", "xhr-update", "admin"])):
+if (empty($login) && in_array($request_access, ["xhr-login", "xhr-logout", "xhr-account", "xhr-add", "xhr-update", "admin"])):
 	// json of invalid login
 	exit; endif;
 
 // Give us the login xhr
-if ($_REQUEST['access'] == "xhr-login"):
+if ($request_access == "xhr-login"):
 
 	// Log in
 
 	exit; endif;
 
 // Give us the logout xhr
-if ($_REQUEST['access'] == "xhr-logout"):
+if ($request_access == "xhr-logout"):
 
 	// Log out
 
@@ -60,28 +132,28 @@ if ($_REQUEST['access'] == "xhr-logout"):
 // Check the database
 
 // Give us the account xhr
-if ($_REQUEST['access'] == "xhr-account"):
+if ($request_access == "xhr-account"):
 
 	// View podcasts
 
 	exit; endif;
 
 // Give us the add xhr
-if ($_REQUEST['access'] == "xhr-add"):
+if ($request_access == "xhr-add"):
 
 	// View podcasts
 
 	exit; endif;
 
 // Give us the update xhr
-if ($_REQUEST['access'] == "xhr-update"):
+if ($request_access == "xhr-update"):
 
 	// View podcasts
 
 	exit; endif;
 
 // Give us the admin panel
-if ($_REQUEST['access'] == "admin"):
+if ($request_access == "admin"):
 
 	amp_header("Admin");
 
@@ -103,21 +175,21 @@ if ($_REQUEST['access'] == "admin"):
 	footer(); endif;
 
 // Give us the episode
-if (array_key_exists($_REQUEST['episode'], $episode_array)):
+if (($request_access == "json") && array_key_exists($request_episode, $episode_array)):
 
 	// Return the audio files
 
 	endif;
 
 // Give us the JSON
-if ($_REQUEST['access'] == "json"):
+if ($request_access == "json"):
 
 	// JSON of all the episodes
 
 	endif;
 
 // Give us the RSS
-if ($_REQUEST['access'] == "rss"):
+if ($request_access == "rss"):
 
 	// Check database 
 
@@ -146,20 +218,6 @@ if ($_REQUEST['access'] == "rss"):
 		</rss>
 
 <? exit; endif;
-
-function amp_header($title_temp) {
-	echo "<html>";
-	echo "<head>";
-	echo "<link rel='alternate' type='application/rss+xml' title='Podcast' href='podcast.ours.foundatoin/podcast.rss' />";
-	echo "<title>". $title_temp ."</title>";
-	echo "</head>";
-	echo "<body>";
-	}
-						   
-function amp_footer() {
-	echo "</body></html>";
-	exit;
-	}
 	
 amp_header($title);
 echo "<h1>". $title ."</h1>";
