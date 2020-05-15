b4e0dac31805083a372f9a7d88a3d61e4fb69b6e
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-05-15 09:34:37 -0700

    Update magic.php

--- magic.php
+++ magic.php
@@ -4,13 +4,5 @@
 
 // Reset a password
 
-// the message
-$msg = "First line of text\nSecond line of text";
-
-// use wordwrap() if lines are longer than 70 characters
-$msg = wordwrap($msg,70);
-
-// send email
-mail("leviclancy@gmail.com","My subject",$msg);
 
 ?>
